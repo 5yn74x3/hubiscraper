1b81a6507f828915458590231cc99fa5816aa456
5yn74x <northern_jh@msn.com>
2024-04-27 20:46:39 +0300

    Update About.tsx

--- frontend/src/About.tsx
+++ frontend/src/About.tsx
@@ -6,9 +6,7 @@ export default function About() {
   return (
     <div className="mx-5 grid min-h-screen md:mx-36">
       <article className="place-items-left mx-auto my-auto flex w-full max-w-6xl flex-col gap-4 rounded-3xl bg-gray-100 p-6 text-jet dark:bg-gun-metal dark:text-bright-gray lg:p-6 xl:p-10">
-       
-        <div>
-          
+   
        
 
       </article>
